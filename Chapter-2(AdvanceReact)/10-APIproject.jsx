14e3ad8e5eada2fa3125056b9783c889bf0bbca9
chetan <chetankumar70174577@gmail.com>
2023-12-28 07:29:55 +0530

    updating file

---
+++ Chapter-2(AdvanceReact)/10-APIproject.jsx
@@ -0,0 +1,42 @@
+import { useState, useEffect } from "react";
+
+const App = () => {
+    return (
+        <GitUsers />
+    )
+}
+
+let GitUsers = () => {
+    let [users, setUsers] = useState([]);
+    useEffect(() => {
+        async function getData() {
+            try {
+                let result = await fetch('https://api.github.com/users')
+                let data = await result.json();
+                setUsers(data);
+            } catch (error) {
+                console.log(error);
+            }
+        }
+        getData();
+    }, [])
+    console.log(users);
+    return <>
+        <h1>Users</h1>
+        {users.map((value, index) => {
+            let { avatar_url, id, login, html_url } = value;
+            return <Component {...value} key={id} />
+        })}
+    </>
+}
+
+let Component = ({ avatar_url, id, login, html_url }) => {
+    return <>
+        <img src={avatar_url}></img>
+        <p style={{ textAlign: 'center' }}>{login}</p>
+        <p style={{ textAlign: 'center' }}>
+            <a href={html_url} >Profile</a>
+        </p>
+    </>
+}
+export default App
