4c37b097b4e34a2e63318523dfecfa8a78bc1200
chetan <chetankumar70174577@gmail.com>
2024-03-17 14:12:58 +0530

    completed load more project

--- ProjectReact/src/LoadMore/index.jsx
+++ ProjectReact/src/LoadMore/index.jsx
@@ -12,13 +12,18 @@ const LoadMore = () => {
         setLoading(true);
         let response = await fetch(
           `https://dummyjson.com/products?limit=20&skip=${
-            count ? 0 : count * 20
+            count === 0 ? 0 : count * 20
           }`
         );
         let data = await response.json();
 
         if (data && data.products && data.products.length) {
-          setProducts(data.products);
+          if (count === 0) {
+            setProducts(data.products);
+            setLoading(false);
+            return;
+          }
+          setProducts((previous) => [...previous, ...data.products]);
           setLoading(false);
         }
       } catch (e) {
@@ -29,17 +34,27 @@ const LoadMore = () => {
     loadFunction();
   }, [count]);
 
+  useEffect(() => {
+    if (products.length === 100) setDisable(true);
+    console.log(products);
+  }, [products]);
+
   if (loading) return <h1>Loading...</h1>;
   return (
     <div>
       {products && products.length
-        ? products.map((product) => (
-            <div>
+        ? products.map((product, index) => (
+            <div key={index}>
               <img src={product.thumbnail} alt={product.title} />
               <h4>{product.title}</h4>
+              <p>{product.id}</p>
             </div>
           ))
         : null}
+      <button disabled={disable} onClick={() => setCount(count + 1)}>
+        Load more
+      </button>
+      {disable && <p>You reach your limit</p>}
     </div>
   );
 };
