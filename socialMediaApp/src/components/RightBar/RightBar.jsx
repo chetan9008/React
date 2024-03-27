53d17d390e71819ceea5eb50ea8a84a4f84f1cc4
chetan <chetankumar70174577@gmail.com>
2024-03-27 13:46:52 +0530

    Rightbar of social media app

--- socialMediaApp/src/components/RightBar/RightBar.jsx
+++ socialMediaApp/src/components/RightBar/RightBar.jsx
@@ -1,5 +1,86 @@
 import "./RightBar.css";
+import gift from "./../../assets/gift.png";
+import ad from "./../../assets/ad.png";
+import freind from "./../../assets/person/3.jpeg";
 const RightBar = () => {
-  return <div className="rightbar">RightBar</div>;
+  return (
+    <div className="rightbar">
+      <div className="rightbarWrapper">
+        <div className="birthdayContainer">
+          <img src={gift} alt={gift} className="birthdayImg" />
+          <span className="birthdayText">
+            <b>Aman</b> and <b>three others</b> have birthday today
+          </span>
+        </div>
+        <img src={ad} alt={ad} className="rightbarAd" />
+        <h4 className="rightbarTitle">Online Friends</h4>
+        <ul className="rightbarFriendList">
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img src={freind} alt={freind} className="rightbarImg" />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarProfileUseraName">John Carter</span>
+          </li>
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img src={freind} alt={freind} className="rightbarImg" />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarProfileUseraName">John Carter</span>
+          </li>
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img src={freind} alt={freind} className="rightbarImg" />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarProfileUseraName">John Carter</span>
+          </li>
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img src={freind} alt={freind} className="rightbarImg" />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarProfileUseraName">John Carter</span>
+          </li>
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img src={freind} alt={freind} className="rightbarImg" />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarProfileUseraName">John Carter</span>
+          </li>
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img src={freind} alt={freind} className="rightbarImg" />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarProfileUseraName">John Carter</span>
+          </li>
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img src={freind} alt={freind} className="rightbarImg" />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarProfileUseraName">John Carter</span>
+          </li>
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img src={freind} alt={freind} className="rightbarImg" />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarProfileUseraName">John Carter</span>
+          </li>
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img src={freind} alt={freind} className="rightbarImg" />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarProfileUseraName">John Carter</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 export default RightBar;
