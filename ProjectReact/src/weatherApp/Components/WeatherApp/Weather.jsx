094766cc72bb320e9594c67d11078385a9f21f51
chetan <chetankumar70174577@gmail.com>
2024-04-07 16:20:00 +0530

    setup icon

--- ProjectReact/src/weatherApp/Components/WeatherApp/Weather.jsx
+++ ProjectReact/src/weatherApp/Components/WeatherApp/Weather.jsx
@@ -4,35 +4,54 @@ import search from "./../../assests/search.png";
 import clear from "./../../assests/clear.png";
 import cloud from "./../../assests/cloud.png";
 import drizzle from "./../../assests/drizzle.png";
-import humidity from "./../../assests/humidity.png";
+import humidityImg from "./../../assests/humidity.png";
 import rain from "./../../assests/rain.png";
 import snow from "./../../assests/snow.png";
 import wind from "./../../assests/wind.png";
 import { useCallback, useEffect, useState } from "react";
 const Weather = () => {
   let [location, setLoaction] = useState("");
+  let [error, setError] = useState(false);
   let apiKey = "5869f8aa5f0553edd0d26781d6c0af29";
   let url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=Metric&appid=${apiKey}`;
   let [status, setStatus] = useState(false);
   let [data, setData] = useState([]);
   let [loading, setLoading] = useState(false);
 
+  if (data.length !== 0) {
+    var {
+      name,
+      main: { temp, humidity },
+      wind: { speed },
+      weather: {
+        [0]: { icon },
+      },
+    } = data;
+  }
+
   let SearchFun = async () => {
     if (location === "") return 0;
     setLoading(true);
     let response = await fetch(url);
+
+    if (response.ok === false) {
+      setError(true);
+      return;
+    }
+
     let Data = await response.json();
     if (Data) {
       setData(Data);
       setLoading(false);
     }
     setStatus(false);
-    console.log(data);
   };
 
   useEffect(() => {
     SearchFun();
   }, [status]);
+
+  if (error) return <h1>Entered city is not found</h1>;
 
   if (loading) return <h1>Loading...</h1>;
 
@@ -58,24 +77,25 @@ const Weather = () => {
         </div>
       </div>
       <div className="weatherImg">
-        <img src={cloud} alt={cloud} />
+        <img
+          src={`http://openweathermap.org/img/w/${icon ? icon : "04d"}.png`}
+          alt={cloud}
+        />
       </div>
-      <div className="weatherTem">
-        {data.length === 0 ? "25'c" : data.main.temp}
-      </div>
-      <div className="weatherLocation">London</div>
+      <div className="weatherTem">{temp ? temp + "\u00b0c" : "25\u00b0c"}</div>
+      <div className="weatherLocation">{name ? name : "London"}</div>
       <div className="data-container">
         <div className="element">
-          <img src={humidity} alt={humidity} className="icon" />
+          <img src={humidityImg} alt={humidityImg} className="icon" />
           <div className="data">
-            <div className="dataUnit">20%</div>
+            <div className="dataUnit">{speed ? speed : 20}%</div>
             <div className="text">humidity</div>
           </div>
         </div>
         <div className="element">
           <img src={wind} alt={wind} className="icon" />
           <div className="data">
-            <div className="dataUnit">80 km/h</div>
+            <div className="dataUnit">{humidity ? humidity : 80} km/h</div>
             <div className="text">wind</div>
           </div>
         </div>
